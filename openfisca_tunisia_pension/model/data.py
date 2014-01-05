4290f5eaf773a2bd15cc9fa91667c0831f666955
Emmanuel Raviart <eraviart@easter-eggs.com>
2014-01-05 15:30:53 +0100

    Replace descriptions with ordered dicts.

--- openfisca_tunisia_pension/model/data.py
+++ openfisca_tunisia_pension/model/data.py
@@ -1,69 +1,74 @@
-# -*- coding:utf-8 -*-
-# Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
+# -*- coding: utf-8 -*-
 
-"""
-openFisca, Logiciel libre de simulation du système socio-fiscal français
-Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
 
-This file is part of openFisca.
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    openFisca is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
 
-    openFisca is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+import collections
 
-    You should have received a copy of the GNU General Public License
-    along with openFisca.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-from openfisca_core.descriptions import ModelDescription
 from openfisca_core.columns import IntCol, EnumCol, BoolCol, AgesCol
 from openfisca_core.utils import Enum
 
 
 QUIFOY = Enum(['vous', 'conj', 'enf1','enf2','enf3','enf4','enf5','enf6','enf7','enf8','enf9'])
 QUIMEN = Enum(['pref', 'cref', 'enf1','enf2','enf3','enf4','enf5','enf6','enf7','enf8','enf9'])
-REG    = Enum(['rsna', 'rsa', 'rsaa', 'rtns', 'rtte', 're', 'rtfr', 'raic', 'cnrps_sal', 'cnrps_pen'])
-
+REG = Enum(['rsna', 'rsa', 'rsaa', 'rtns', 'rtte', 're', 'rtfr', 'raic', 'cnrps_sal', 'cnrps_pen'])
 
 
 # raic -> raci
 
-class InputDescription(ModelDescription):
-    '''
-    Socio-economic data
-    Donnée d'entrée de la simulation à fournir à partir d'une enquète ou
-    à générer avec un générateur de cas type
-    '''
+# Socio-economic data
+# Donnée d'entrée de la simulation à fournir à partir d'une enquète ou
+# à générer avec un générateur de cas type
+column_by_name = collections.OrderedDict((
+    ('noi', IntCol()),
+    ('idmen', IntCol()),
+    ('idfoy', IntCol()),
 
-    noi = IntCol()
-    idmen = IntCol()
-    idfoy = IntCol()
+    ('quimen', EnumCol(QUIMEN)),
+    ('quifoy', EnumCol(QUIFOY)),
 
-    quimen  = EnumCol(QUIMEN)
-    quifoy  = EnumCol(QUIFOY)
+    ('statmarit', BoolCol()),
+    ('loyer', IntCol()),
 
-    statmarit = BoolCol()
-    loyer = IntCol()
+    ('sal0', IntCol()),
+    ('sal1', IntCol()),
+    ('sal2', IntCol()),
+    ('sal3', IntCol()),
+    ('sal4', IntCol()),
+    ('sal5', IntCol()),
+    ('sal6', IntCol()),
+    ('sal7', IntCol()),
+    ('sal8', IntCol()),
+    ('sal9', IntCol()),
+    ('sal10', IntCol()),
 
-    sal0 = IntCol()
-    sal1 = IntCol()
-    sal2 = IntCol()
-    sal3 = IntCol()
-    sal4 = IntCol()
-    sal5 = IntCol()
-    sal6 = IntCol()
-    sal7 = IntCol()
-    sal8 = IntCol()
-    sal9 = IntCol()
-    sal10 = IntCol()
+    ('age', AgesCol(default = 65)),
+    ('nb_trim_val', IntCol()),
+    ('regime', EnumCol(REG)),
+    ))
 
-    age = AgesCol(default=65)
-    nb_trim_val = IntCol()
-    regime = EnumCol(REG)
-    
+for name, column in column_by_name.iteritems():
+    if column.label is None:
+        column.label = name
+    assert column.name is None
+    column.name = name
--- openfisca_tunisia_pension/model/model.py
+++ openfisca_tunisia_pension/model/model.py
@@ -1,46 +1,50 @@
-# -*- coding:utf-8 -*-Boll
-# Copyright © 2012 Clément Schaff, Mahdi Ben Jelloul
-
-"""
-OpenFiscaTn, Logiciel libre de simulation du système socio-fiscal tunisien
-Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
-
-This file is part of openFisca.
-
-    openFisca is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    openFisca is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with openFisca.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# -*- coding: utf-8 -*-
 
 
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import collections
 from datetime import date
 
 from openfisca_core.columns import Prestation, BoolPresta
-from openfisca_core.descriptions import ModelDescription
 
 from . import pension
 
 
-class OutputDescription(ModelDescription):
-    """
-    Model description
-    """
-
+prestation_by_name = collections.OrderedDict((
     ############################################################
     # Pensions
     ############################################################
 
-    sal_ref_rsna = Prestation(pension._sal_ref_rsna, entity="ind", label=u"Salaire de référence")
-    pension_rsna = Prestation(pension._pension_rsna, entity="ind", label=u"Pension de retraite")
+    ('sal_ref_rsna', Prestation(pension._sal_ref_rsna, entity = "ind", label = u"Salaire de référence")),
+    ('pension_rsna', Prestation(pension._pension_rsna, entity = "ind", label = u"Pension de retraite")),
 
-    sal_ref_rsa = Prestation(pension._sal_ref_rsa, entity="ind", label=u"Salaire de référence")
-    pension_rsa = Prestation(pension._pension_rsa, entity="ind", label=u"Pension de retraite")
+    ('sal_ref_rsa', Prestation(pension._sal_ref_rsa, entity = "ind", label = u"Salaire de référence")),
+    ('pension_rsa', Prestation(pension._pension_rsa, entity = "ind", label = u"Pension de retraite")),
+    ))
+
+for name, prestation in prestation_by_name.iteritems():
+    if prestation.label is None:
+        prestation.label = name
+    assert prestation.name is None
+    prestation.name = name
