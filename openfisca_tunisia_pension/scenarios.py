2130b69ea0ba0fcf5e2e3b1906a659f2d7de0c90
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-12 15:46:37 +0100

    Add yaml tests

--- openfisca_tunisia_pension/scenarios.py
+++ openfisca_tunisia_pension/scenarios.py
@@ -1,28 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-# OpenFisca -- A versatile microsimulation software
-# By: OpenFisca Team <contact@openfisca.fr>
-#
-# Copyright (C) 2011, 2012, 2013, 2014, 2015 OpenFisca Team
-# https://github.com/openfisca
-#
-# This file is part of OpenFisca.
-#
-# OpenFisca is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# OpenFisca is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
 import collections
 import datetime
 import itertools
@@ -33,8 +11,11 @@ import uuid
 from openfisca_core import conv, scenarios
 
 
+def N_(message):
+    return message
+
+
 log = logging.getLogger(__name__)
-N_ = lambda message: message
 year_or_month_or_day_re = re.compile(ur'(18|19|20)\d{2}(-(0[1-9]|1[0-2])(-([0-2]\d|3[0-1]))?)?$')
 
 
@@ -481,8 +462,13 @@ class Scenario(scenarios.AbstractScenario):
         return json_or_python_to_test_case
 
     def suggest(self):
-        period_start_year = self.period.start.year
+        """Returns a dict of suggestions and modifies self.test_case applying those suggestions."""
         test_case = self.test_case
+        if test_case is None:
+            return None
+
+        period_start_date = self.period.start.date
+        period_start_year = self.period.start.year
         suggestions = dict()
 
         for individu in test_case['individus']:
--- openfisca_tunisia_pension/tests/test_pension.py
+++ openfisca_tunisia_pension/tests/test_pension.py
@@ -1,41 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
-# OpenFisca -- A versatile microsimulation software
-# By: OpenFisca Team <contact@openfisca.fr>
-#
-# Copyright (C) 2011, 2012, 2013 OpenFisca Team
-# https://github.com/openfisca/openfisca
-#
-# This file is part of OpenFisca.
-#
-# OpenFisca is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# OpenFisca is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-# The model variables are created by each country-specific package (cf function init_country())
-# Note: The variables below are not inited (to None) here, to ensure that execution will fail when they are used before
-# OpenFisca country-specific package is properly inited.# -*- coding:utf-8 -*-
-# Created on 14 mai 2013
-# This file is part of OpenFisca.
-# OpenFisca is a socio-fiscal microsimulation software
-# Copyright ©2013 Clément Schaff, Mahdi Ben Jelloul
-# Licensed under the terms of the GVPLv3 or later license
-# (see openfisca/__init__.py for details)
-
-
-import datetime
-
 from openfisca_core import periods
 from openfisca_core.tools import assert_near
 from . import base
@@ -52,7 +16,7 @@ def test_rsna():
         #     )],
         period = periods.period(year),
         parent1 = dict(
-            age = 60,  # birth = datetime.date(year - 60, 1, 1),
+            age = 60,
             nb_trim_val = 50,
             salaire = dict(
                 [("{}".format(yr + 1), 12 * 1000) for yr in range(2014 - 40, 2014)]
@@ -64,10 +28,5 @@ def test_rsna():
     assert_near(simulation.calculate_add('pension_rsna'), 5400, 1)
 
 
-
 if __name__ == '__main__':
-
     test_rsna()
-
-#    nose.core.runmodule(argv=[__file__, '-v', '-i test_*.py'])
-#     nose.core.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'], exit=False)
