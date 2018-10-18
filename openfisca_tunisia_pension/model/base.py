c9c9a39f4d568159a6b37a5d63f90fb53e3f1f0c
sandcha <sandcha@users.noreply.github.com>
2018-10-18 23:23:59 +0200

    Import entities from tunisia country package

--- openfisca_tunisia_pension/model/base.py
+++ openfisca_tunisia_pension/model/base.py
@@ -3,4 +3,4 @@
 
 from datetime import date
 from openfisca_core.model_api import *
-from openfisca_tunisia_pension.entities import FoyerFiscal, Individu, Menage
+from openfisca_tunisia.entities import FoyerFiscal, Individu, Menage
