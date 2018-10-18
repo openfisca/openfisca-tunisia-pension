f92e2e7687767a1e224018ab1467c2359c484a69
sandcha <sandcha@users.noreply.github.com>
2018-10-19 00:00:55 +0200

    Refactor tests tax_benefit_system init

--- openfisca_tunisia_pension/tests/base.py
+++ openfisca_tunisia_pension/tests/base.py
@@ -1,10 +1,10 @@
-from openfisca_tunisia_pension import TunisiaPensionTaxBenefitSystem
+from openfisca_tunisia import TunisiaTaxBenefitSystem
+
+# Extend tunisia tax_benefit_system
+tax_benefit_system = TunisiaTaxBenefitSystem()
+tax_benefit_system.load_extension("openfisca_tunisia_pension")
 
 
 __all__ = [
     'tax_benefit_system',
-    'TunisiaPensionTaxBenefitSystem',
     ]
-
-
-tax_benefit_system = TunisiaPensionTaxBenefitSystem()
