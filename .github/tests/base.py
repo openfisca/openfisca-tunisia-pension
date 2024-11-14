c197c071c9ac7b7aff07c6c8920d42dd9df39e3d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2024-11-14 12:39:15 +0100

    Move away tests from package

---
+++ .github/tests/base.py
@@ -0,0 +1,10 @@
+from openfisca_tunisia_pension import TunisiaPensionTaxBenefitSystem
+
+
+__all__ = [
+    'tax_benefit_system',
+    'TunisiaPensionTaxBenefitSystem',
+    ]
+
+
+tax_benefit_system = TunisiaPensionTaxBenefitSystem()
